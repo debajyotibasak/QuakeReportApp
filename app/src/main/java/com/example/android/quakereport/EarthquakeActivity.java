58cad11774824092abbda1ce8cc7c1476bedaa61
debo1994 <d.basak.db@gmail.com>
2017-02-16 16:21:42 +0530

    Getting data from JSON. Showing Date and Time in Format

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -31,15 +31,8 @@ public class EarthquakeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(new Earthquake("7.2", "San Francisco", "Feb 2 2017"));
-        earthquakes.add(new Earthquake("7.2", "San Francisco", "Feb 2 2017"));
-        earthquakes.add(new Earthquake("7.2", "San Francisco", "Feb 2 2017"));
-        earthquakes.add(new Earthquake("7.2", "San Francisco", "Feb 2 2017"));
-        earthquakes.add(new Earthquake("7.2", "San Francisco", "Feb 2 2017"));
-        earthquakes.add(new Earthquake("7.2", "San Francisco", "Feb 2 2017"));
-        earthquakes.add(new Earthquake("7.2", "San Francisco", "Feb 2 2017"));
+        // get the list of earthquakes from Query Utils
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
